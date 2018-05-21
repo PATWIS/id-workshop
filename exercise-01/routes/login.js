f93a3de68d4a3489ca86cbdc967382f8dc334e0a
PATWIS <patryk.wisniewski@outlook.com>
2018-05-21 12:59:15 +0200

    Add the sign-up form

--- exercise-01/routes/login.js
+++ exercise-01/routes/login.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('login', { user: req.user });
+});
+
 module.exports = router;
