c6541b912f3ab1c8cd5cdebe33a0243a0fc97e21
PATWIS <patryk.wisniewski@outlook.com>
2018-05-21 12:56:25 +0200

    Registering the user

--- exercise-01/routes/signup.js
+++ exercise-01/routes/signup.js
@@ -4,4 +4,36 @@ const db = require('../data');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('signup', { user: req.user });
+});
+
+router.post('/', (req, res, next) => {
+  if (!req.body.login) {
+    return next(new Error('Login is missing'));
+  }
+  if (!req.body.password) {
+    return next(new Error('Password is missing'));
+  }
+  if (req.body.password !== req.body.confirm_password) {
+    return next(new Error('Passwords don\'t match'));
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      db.register(req.body.login, hash, (err, ok) => {
+        if (err) {
+          return next(err);
+        }
+        if (!ok) {
+          return next(new Error(`User ${req.body.login} already exists`));
+        }
+        
+        res.redirect('/');
+      });
+    });
+  });
+});
+
+
 module.exports = router;
